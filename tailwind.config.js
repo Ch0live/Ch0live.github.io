30b6e0390f1ee765fc8dc72e80dd4d37a1bcd0f2
Ch0live <colive@scottlogic.com>
2022-10-20 13:37:09 +0100

    Add house style sheet & styling
    - Add image to splash screen
    - Improve colours of all pages
    - Add extra page for displaying house style

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./index.html"
   ],
   theme: {
-    extend: {},
+    screens: {},
+    colors: {
+      'background-orange': '#D4965A',
+      'loaf-mug-orange': '#CFAE61',
+    }, 
+    fontFamily: {},
+    extend: {}
   },
   plugins: [],
 }
