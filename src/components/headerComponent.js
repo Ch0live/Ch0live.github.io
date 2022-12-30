efea359074523edfac11e82ff0442c4ae6e4313f
Ch0live <colive@scottlogic.com>
2022-12-30 17:45:18 +0000

    Clean CSS for blog homepage and post

--- src/components/headerComponent.js
+++ src/components/headerComponent.js
@@ -1,20 +1,20 @@
 class HeaderComponent extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = `<header>
+        this.innerHTML = `
         <nav>
           <ul id="nav">
             <li>
               <a class="text-grey-500 hover:text-black font-medium " href="../index.html">Splash Page</a>
             </li>
             <li>
-              <a class="text-grey-500 hover:text-black font-medium " href="./blog.html">Blog</a>
+              <a class="text-grey-500 hover:text-black font-medium " href="./blog.html">Home</a>
             </li>
             <li>
               <a class="text-grey-500 hover:text-black font-medium " href="./exampleBlogPost.html"><em>An Introduction to Markdown</em></a>
             </li>
           </ul>
         </nav>
-      </header>`;
+      `;
     }
 }
 
