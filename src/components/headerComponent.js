f86f6ec017950806449e3862700531b257afe5e4
Ch0live <charlesjolive@gmail.com>
2022-12-30 10:28:24 +0000

    Add header pure CSS

--- src/components/headerComponent.js
+++ src/components/headerComponent.js
@@ -2,14 +2,14 @@ class HeaderComponent extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `<header>
         <nav>
-          <ul class="flex bg-loaf-mug-orange">
-            <li class="mx-4 mt-2">
+          <ul id="nav">
+            <li>
               <a class="text-grey-500 hover:text-black font-medium " href="../index.html">Splash Page</a>
             </li>
-            <li class="mx-4 mt-2">
+            <li>
               <a class="text-grey-500 hover:text-black font-medium " href="./blog.html">Blog</a>
             </li>
-            <li class="mx-4 mt-2">
+            <li>
               <a class="text-grey-500 hover:text-black font-medium " href="./exampleBlogPost.html"><em>An Introduction to Markdown</em></a>
             </li>
           </ul>
