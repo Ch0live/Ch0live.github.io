7fc98edff08a65f70081892dd3e0c27247e65a26
Ch0live <charlesjolive@gmail.com>
2023-01-07 15:28:17 +0000

    Add padding outside all page content
    - Remove Tailwind classNames from header component
    - rename to blogPost.html

--- src/components/headerComponent.js
+++ src/components/headerComponent.js
@@ -4,13 +4,13 @@ class HeaderComponent extends HTMLElement {
         <nav>
           <ul id="nav">
             <li>
-              <a class="text-grey-500 hover:text-black font-medium " href="../index.html">Splash Page</a>
+              <a href="../index.html">Splash Page</a>
             </li>
             <li>
-              <a class="text-grey-500 hover:text-black font-medium " href="./blog.html">Home</a>
+              <a href="./blog.html">Home</a>
             </li>
             <li>
-              <a class="text-grey-500 hover:text-black font-medium " href="./exampleBlogPost.html"><em>An Introduction to Markdown</em></a>
+              <a href="./blogPost.html"><em>An Introduction to Markdown</em></a>
             </li>
           </ul>
         </nav>
