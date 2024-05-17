c4108027b78749914443271e58ed1f290360ea36
ch0live <dozeyis@gmail.com>
2024-05-17 08:07:33 +0100

    Add wrapper template

--- converter.py
+++ converter.py
@@ -1,4 +1,11 @@
 import os
+from jinja2 import FileSystemLoader, Environment
+
+env = Environment(loader=FileSystemLoader("./templates"))
+
+
+def get_template(name: str):
+    return env.get_template(f"{name}.j2")
 
 
 def interpret_md(line: str):
@@ -6,13 +13,15 @@ def interpret_md(line: str):
 
 
 def generate_html_from_md(name: str, category: str):
-
     path = os.path.join(os.getcwd(), "blog-posts", category, f"{name}.md")
     md_file = open(path, "r")
    
     html_file_body = []
     for line in md_file.readlines():
         html_file_body.append(interpret_md(line))
+
+    temp = get_template("wrapper").render(body=html_file_body)
+    print(temp)
 
 
 if __name__ == "__main__":
