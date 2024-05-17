3057b1110f16b9ca7187948624c2c422048042b8
ch0live <dozeyis@gmail.com>
2024-05-17 08:14:47 +0100

    Write to file

--- converter.py
+++ converter.py
@@ -4,12 +4,21 @@ from jinja2 import FileSystemLoader, Environment
 env = Environment(loader=FileSystemLoader("./templates"))
 
 
+def write_to_file(name: str, body: str):
+    f = open(f"{name}.html", "a")
+    f.write(body)
+    f.close()
+
+
 def get_template(name: str):
     return env.get_template(f"{name}.j2")
 
 
 def interpret_md(line: str):
-    return f"<p> hello! Here's your line: {line} </p>"
+    if line[0:2] == "# ":
+        return get_template("h1").render(line[2:])
+    else:
+        return "<p> placeholder </p>"
 
 
 def generate_html_from_md(name: str, category: str):
@@ -20,8 +29,8 @@ def generate_html_from_md(name: str, category: str):
     for line in md_file.readlines():
         html_file_body.append(interpret_md(line))
 
-    temp = get_template("wrapper").render(body=html_file_body)
-    print(temp)
+    html_file = get_template("wrapper").render(body=html_file_body)
+    write_to_file(name, html_file)
 
 
 if __name__ == "__main__":
