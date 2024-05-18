2c5c58d218d43ab22a7da7953d77326ebc08b02a
ch0live <dozeyis@gmail.com>
2024-05-18 08:37:31 +0100

    Add content interpretation (with j2 templates)

--- blog-posts/travel/charging-and-walking.md
+++ blog-posts/travel/charging-and-walking.md
@@ -1,3 +1,4 @@
+# Charging and walking
 In past holidays I have often just taken a single European plug adapter, converting straight to UK and powering 1 or maybe 2 at most devices. However, with the upcoming trip likely having lots of moments where I don't have access to power for more than a 30-minute stop in a café, an efficient and fast way to charge all my bits n' bobs is required.
 
 Based on about 10 minutes of research I have purchased a Wizco universal plug adapter. It's full name (like most Amazon products attempting to get to the top of the search result) is a lot longer and full of buzz words so I've shortened it. 
@@ -11,6 +12,8 @@ Above are the things I wish to carry with me as I go, plus my phone which I used
 ```
 
 I'd consider adding 20-30% to all my devices in a 30 minute pit-spot enough to manage on. Note I was using the laptop during the entirety of this experiment and also had the phone charged using the "Warp Charger". I'd assumed giving it the main plug socket port with it's own wall wart would give it it's standard 60w charge output and it looks like I was right. Also note that the battery pack can only say out of 4 how charged it is, but increasing it 1/4 is the same as adding ~50% to my phone indirectly. Also, as the earphones don't have a battery charge amount I don't have any other metric other than it changing from red to green on the front facing LED. Even though I have no idea what the charge was at the start, it's nice to know it was charging as it did manage to reach full charge.It seems this charger will in fact hold up well enough.
+
+### New boots
 
 ![[meindlBoots.jpg]]
 
--- converter.py
+++ converter.py
@@ -1,21 +1,21 @@
+from io import TextIOWrapper
 import os
 import re
-from jinja2 import FileSystemLoader, Environment
+from jinja2 import FileSystemLoader, Environment, Template
 
 env = Environment(loader=FileSystemLoader("./templates"))
 
 
-def write_to_file(name: str, body: str):
-    f = open(f"{name}.html", "a")
-    f.write(body)
-    f.close()
+def write_to_file(path: str, html_file: str):
+    with open(path + ".html", 'w') as file:
+        file.write(html_file)
 
 
-def get_template(name: str):
+def get_template(name: str) -> Template:
     return env.get_template(f"{name}.j2")
 
 
-def interpret_md(line: str):
+def interpret_content(line: str) -> str:
     if line[0:2] == "# ":
         return get_template("block").render(type="h1", content=line[2:])
     if line[0:3] == "## ":
@@ -23,22 +23,26 @@ def interpret_md(line: str):
     if line[0:4] == "### ":
         return get_template("block").render(type="h3", content=line[4:])
     if re.search("\!\[\[.*", line):
-        return get_template("img").render(filename=line[3:-3])
+        return get_template("img").render(filename=line[3:-2])
     else:
         return get_template("block").render(type="p", content=line)
+
+
+def interpret_md(md_file: TextIOWrapper) -> str:
+    html_file_body = []
+    for line in md_file.readlines():
+        if re.search("^\s*$", line):
+            continue
+        html_file_body.append(interpret_content(line.strip()))
+    return html_file_body
 
 
 def generate_html_from_md(name: str, category: str):
     path = os.path.join(os.getcwd(), "blog-posts", category, name)
     md_file = open(path + ".md", "r")
-   
-    html_file_body = []
-    for line in md_file.readlines():
-        html_file_body.append(interpret_md(line))
-
+    html_file_body = interpret_md(md_file)
     html_file = get_template("wrapper").render(body=html_file_body)
-    with open(path + ".html", 'w') as file:
-        file.write(html_file)
+    write_to_file(path, html_file)
     print(f"written to path {path}")
 
 
