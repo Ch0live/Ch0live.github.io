a9f11c9c4fac33d1c6d01e0f8311de83e5bbc03d
ch0live <dozeyis@gmail.com>
2024-05-18 08:09:58 +0100

    Write lines to file (unformatted)

--- converter.py
+++ converter.py
@@ -1,4 +1,5 @@
 import os
+import re
 from jinja2 import FileSystemLoader, Environment
 
 env = Environment(loader=FileSystemLoader("./templates"))
@@ -16,21 +17,29 @@ def get_template(name: str):
 
 def interpret_md(line: str):
     if line[0:2] == "# ":
-        return get_template("h1").render(line[2:])
+        return get_template("block").render(type="h1", content=line[2:])
+    if line[0:3] == "## ":
+        return get_template("block").render(type="h2", content=line[3:])
+    if line[0:4] == "### ":
+        return get_template("block").render(type="h3", content=line[4:])
+    if re.search("\!\[\[.*", line):
+        return get_template("img").render(filename=line[3:-3])
     else:
-        return "<p> placeholder </p>"
+        return get_template("block").render(type="p", content=line)
 
 
 def generate_html_from_md(name: str, category: str):
-    path = os.path.join(os.getcwd(), "blog-posts", category, f"{name}.md")
-    md_file = open(path, "r")
+    path = os.path.join(os.getcwd(), "blog-posts", category, name)
+    md_file = open(path + ".md", "r")
    
     html_file_body = []
     for line in md_file.readlines():
         html_file_body.append(interpret_md(line))
 
     html_file = get_template("wrapper").render(body=html_file_body)
-    write_to_file(name, html_file)
+    with open(path + ".html", 'w') as file:
+        file.write(html_file)
+    print(f"written to path {path}")
 
 
 if __name__ == "__main__":
